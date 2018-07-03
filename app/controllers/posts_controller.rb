8ff1e034cfbe8f39e5659e049064f1a7f7771365
Victoria Meng <nights1ststar@protonmail.com>
2018-07-03 23:28:33 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,8 @@ class PostsController < ApplicationController
     if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
-    else 
+    else
+      render :edit
     end
   end
 
